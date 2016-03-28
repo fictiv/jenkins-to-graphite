0f8cb694ad2e4137f9d84e057c2ed7228a7f5fd5
Jim Ruga <jim@fictiv.com>
2016-03-28 12:10:15 -0700

    framing out support for cloudwatch

--- jenkins-to-graphite.py
+++ jenkins-to-graphite.py
@@ -25,6 +25,7 @@ import sys
 import socket
 import time
 import urllib2
+import boto.ec2.cloudwatch
 
 try:
     # this should be available in any python 2.6 or newer
@@ -77,13 +78,29 @@ class JenkinsServer(object):
     def get_data(self, url):
         return self.get_raw_data("%s/api/json" % url)
 
+class Debug(object):
+    def __init__(self):
+        self.data={}
+
+    def add_data(self, key, value):
+        self.data["%s.%s" % (self.prefix, key)] = value
+
+    def _data_as_msg(self):
+        msg = ""
+        now = time.time()
+        for (key, val) in self.data.items():
+            msg += "%s %s %s\n" % (key, val, now)
+        return msg
+
+    def send(self):
+        print self._data_as_msg()
+        return True
 
 class GraphiteServer(object):
     def __init__(self, server, port, prefix):
         self.server = server
         self.port = int(port)
         self.prefix = prefix.rstrip('.')
-
         self.data = {}
 
     def add_data(self, key, value):
@@ -101,7 +118,6 @@ class GraphiteServer(object):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((self.server, self.port))
             s.sendall(self._data_as_msg())
-#            print self._data_as_msg()
             s.close()
         except Exception, e:
             logging.warn("Unable to send msg to graphite: %s" % (e,))
@@ -109,6 +125,30 @@ class GraphiteServer(object):
 
         return True
 
+class CloudwatchServer(object):
+    def __init__(self):
+        self.namespace = "Jenkins"
+        self.data={}
+
+    def add_data(self, key, value):
+        self.data["%s.%s" % (self.prefix, key)] = value
+
+    def _data_as_msg(self):
+        msg = ""
+        now = time.time()
+        for (key, val) in self.data.items():
+            msg += "%s %s %s\n" % (key, val, now)
+        return msg
+
+    def send(self):
+        try:
+            cwc = boto.ec2.cloudwatch.connect_to_region(boto.cfg.cloudwatch_region_name)
+#            cwc.put_metric_data(self.namespace, name, value=None, timestamp=None, unit=None, dimensions=None, statistics=None)
+        except Exception, e:
+            logging.warn("Unable to send msg to cloudwatch: %s" % (e,))
+            return False
+
+        return True
 
 def parse_args():
     parser = optparse.OptionParser()
@@ -129,6 +169,8 @@ def parse_args():
                       help="Graphite metric prefix")
     parser.add_option("", "--label", action="append", dest="labels",
                       help="Fetch stats applicable to this node label. Can bee applied multiple times for monitoring more labels.")
+    parser.add_option("", "--debug-output",
+                      help="Set to True or False - when set to False output the stats and do not send to Cloudwatch.")
 
     (opts, args) = parser.parse_args()
 
@@ -143,54 +185,61 @@ def main():
     opts = parse_args()
     jenkins = JenkinsServer(opts.jenkins_url, opts.jenkins_user,
                             opts.jenkins_password)
-    graphite = GraphiteServer(opts.graphite_server, opts.graphite_port,
-                              opts.prefix)
+
+    task = Debug()
+
+    if (opts.graphite_server and not opts.debug):
+        task = GraphiteServer(opts.graphite_server, opts.graphite_port,
+                                  opts.prefix)
+
+    if (opts.cloudwatch_upload and not opts.debug):
+        task = CloudwatchServer()
 
     executor_info = jenkins.get_data("computer")
     queue_info = jenkins.get_data("queue")
     build_info_min = jenkins.get_raw_data("view/All/timeline/data?min=%d&max=%d" % ((time.time() - 60) * 1000, time.time() * 1000))
     build_info_hour = jenkins.get_raw_data("view/All/timeline/data?min=%d&max=%d" % ((time.time() - 3600) * 1000, time.time() * 1000))
 
-    graphite.add_data("queue.size", len(queue_info.get("items", [])))
+    task.add_data("queue.size", len(queue_info.get("items", [])))
 
-    graphite.add_data("builds.started_builds_last_minute", len(build_info_min.get("events", [])))
-    graphite.add_data("builds.started_builds_last_hour", len(build_info_hour.get("events", [])))
+    task.add_data("builds.started_builds_last_minute", len(build_info_min.get("events", [])))
+    task.add_data("builds.started_builds_last_hour", len(build_info_hour.get("events", [])))
 
-    graphite.add_data("executors.total", executor_info.get("totalExecutors", 0))
-    graphite.add_data("executors.busy", executor_info.get("busyExecutors", 0))
-    graphite.add_data("executors.free",
+    task.add_data("executors.total", executor_info.get("totalExecutors", 0))
+    task.add_data("executors.busy", executor_info.get("busyExecutors", 0))
+    task.add_data("executors.free",
                       executor_info.get("totalExecutors", 0) -
                       executor_info.get("busyExecutors", 0))
 
     nodes_total = executor_info.get("computer", [])
     nodes_offline = [j for j in nodes_total if j.get("offline")]
-    graphite.add_data("nodes.total", len(nodes_total))
-    graphite.add_data("nodes.offline", len(nodes_offline))
-    graphite.add_data("nodes.online", len(nodes_total) - len(nodes_offline))
+    task.add_data("nodes.total", len(nodes_total))
+    task.add_data("nodes.offline", len(nodes_offline))
+    task.add_data("nodes.online", len(nodes_total) - len(nodes_offline))
 
     if opts.labels:
         for label in opts.labels:
             label_info = jenkins.get_data("label/%s" % label)
-            graphite.add_data("labels.%s.jobs.tiedJobs" % label, len(label_info.get("tiedJobs", [])))
-            graphite.add_data("labels.%s.nodes.total" % label, len(label_info.get("nodes", [])))
-            graphite.add_data("labels.%s.executors.total" % label, label_info.get("totalExecutors", 0))
-            graphite.add_data("labels.%s.executors.busy" % label, label_info.get("busyExecutors", 0))
-            graphite.add_data("labels.%s.executors.free" % label,
+            task.add_data("labels.%s.jobs.tiedJobs" % label, len(label_info.get("tiedJobs", [])))
+            task.add_data("labels.%s.nodes.total" % label, len(label_info.get("nodes", [])))
+            task.add_data("labels.%s.executors.total" % label, label_info.get("totalExecutors", 0))
+            task.add_data("labels.%s.executors.busy" % label, label_info.get("busyExecutors", 0))
+            task.add_data("labels.%s.executors.free" % label,
                               label_info.get("totalExecutors", 0) -
                               label_info.get("busyExecutors", 0))
 
     if opts.jobs:
-        builds_info = jenkins.get_data("/view/%s" % opts.jobs)
+        builds_info = jenkins.get_data("/jobs/%s" % opts.jobs)
         jobs = builds_info.get("jobs", [])
         ok = [j for j in jobs if j.get("color", 0) == "blue"]
         fail = [j for j in jobs if j.get("color", 0) == "red"]
         warn = [j for j in jobs if j.get("color", 0) == "yellow"]
-        graphite.add_data("jobs.total", len(jobs))
-        graphite.add_data("jobs.ok", len(ok))
-        graphite.add_data("jobs.fail", len(fail))
-        graphite.add_data("jobs.warn", len(warn))
+        task.add_data("jobs.total", len(jobs))
+        task.add_data("jobs.ok", len(ok))
+        task.add_data("jobs.fail", len(fail))
+        task.add_data("jobs.warn", len(warn))
 
-    graphite.send()
+    task.send()
 
 if __name__ == "__main__":
     main()
