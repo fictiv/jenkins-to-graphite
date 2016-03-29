b4940cb8b90031873df5ad37bb061fb21a46d223
Jim Ruga <jim@fictiv.com>
2016-03-28 21:24:27 -0700

    more debugging

--- jenkins-to-graphite.py
+++ jenkins-to-graphite.py
@@ -158,9 +158,10 @@ class CloudwatchServer(object):
         try:
             now = time.time()
             cwc = boto.ec2.cloudwatch.connect_to_region(self.region)
+
             for (key, val) in self.data.items():
-                cwc.put_metric_data(self.namespace, key, value=val, timestamp=now, unit='Count')
-                # cwc.put_metric_data(self.namespace, key, value=val, timestamp=now, unit=Count, dimensions=None, statistics=None)
+                cwc.put_metric_data(namespace=self.namespace, name=key, value=val, timestamp=now, unit="Count")
+
         except Exception, e:
             logging.warn("Unable to send msg to cloudwatch: %s" % (e,))
             return False
