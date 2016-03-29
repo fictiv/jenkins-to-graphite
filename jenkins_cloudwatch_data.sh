2e5e593b8179581d8d306b1d0d157aa9f58ded52
Jim Ruga <jim@fictiv.com>
2016-03-28 22:03:44 -0700

    adding a shell script to walk the jobs

---
+++ jenkins_cloudwatch_data.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+export HOME=/home/ubuntu
+export PATH=/home/ubuntu/perl5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-7-openjdk-amd64/jre/bin:/home/ubuntu/CloudWatch-1.0.20.0/bin
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
+export PATH=$PATH:$JAVA_HOME/bin
+export AWS_CLOUDWATCH_HOME=/home/ubuntu/CloudWatch-1.0.20.0
+export PATH=$PATH:$AWS_CLOUDWATCH_HOME/bin
+export AWS_CLOUDWATCH_URL=http://monitoring.us-west-2.amazonaws.com/
+
+. $HOME/.profile
+
+cmd="./jenkins-to-graphite.py --jenkins-url=https://prod-pipeline.fictiv.com/jenkins/ --jenkins-user=jim@fictiv.com --jenkins-password=ebec5039c7f6dec7a9e0d79d311376fc --job=pipeline-model-v4"
+
+exec $cmd 2>&1 | /usr/bin/logger -t pipeline_model_v4_cloudwatch_data
+
+cmd="./jenkins-to-graphite.py --jenkins-url=https://prod-pipeline.fictiv.com/jenkins/ --jenkins-user=jim@fictiv.com --jenkins-password=ebec5039c7f6dec7a9e0d79d311376fc --job=worker-3dp-job-print-material"
+
+exec $cmd 2>&1 | /usr/bin/logger -t worker_3dp_job_print_material_cloudwatch_data
+
+cmd="./jenkins-to-graphite.py --jenkins-url=https://prod-pipeline.fictiv.com/jenkins/ --jenkins-user=jim@fictiv.com --jenkins-password=ebec5039c7f6dec7a9e0d79d311376fc --job=worker-3dp-print-material"
+
+exec $cmd 2>&1 | /usr/bin/logger -t worker_3dp_print_material_cloudwatch_data
+
+cmd="./jenkins-to-graphite.py --jenkins-url=https://prod-pipeline.fictiv.com/jenkins/ --jenkins-user=jim@fictiv.com --jenkins-password=ebec5039c7f6dec7a9e0d79d311376fc --job=worker-3dp-geometry"
+
+exec $cmd 2>&1 | /usr/bin/logger -t worker_3dp_geometry_cloudwatch_data
+
+cmd="./jenkins-to-graphite.py --jenkins-url=https://prod-pipeline.fictiv.com/jenkins/ --jenkins-user=jim@fictiv.com --jenkins-password=ebec5039c7f6dec7a9e0d79d311376fc --job=worker-cnc-geometry"
+
+exec $cmd 2>&1 | /usr/bin/logger -t worker_3dp_geometry_cloudwatch_data
+#exec $cmd ${@:+"$@"}
