96ee0e917629f97a6fee98392c93ecb06a13e7f5
Jim Ruga <jim@fictiv.com>
2016-03-28 20:56:15 -0700

    formatting the values as ints

--- jenkins-to-graphite.py
+++ jenkins-to-graphite.py
@@ -159,7 +159,7 @@ class CloudwatchServer(object):
             now = time.time()
             cwc = boto.ec2.cloudwatch.connect_to_region(self.region)
             for (key, val) in self.data.items():
-                cwc.put_metric_data(self.namespace, key, value=val, timestamp=now, unit='Count')
+                cwc.put_metric_data(self.namespace, key, value=int(val), timestamp=now, unit='Count')
                 # cwc.put_metric_data(self.namespace, key, value=val, timestamp=now, unit=Count, dimensions=None, statistics=None)
         except Exception, e:
             logging.warn("Unable to send msg to cloudwatch: %s" % (e,))
